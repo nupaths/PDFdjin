d0f2fedd481be1eddac6afacd4eb0feac97fb602
lmease <86120939+lmease@users.noreply.github.com>
2023-04-21 22:01:07 -0400

    042123 Pull request client changes
    Lauren Completed all initial changes requested by municipality client.

--- script.js
+++ script.js
@@ -318,3 +318,18 @@ function spawnWindow(){
   document.body.appendChild(clone);
   console.log("I'm in this function")
 }
+
+function myFunction3() {
+	var body = document.getElementsByTagName("BODY")[0];
+	body.classList.toggle("night");
+	var html = document.getElementsByTagName("HTML")[0];
+	html.classList.toggle("night");
+	var sun = document.getElementById("sun");
+	sun.classList.toggle("gone");
+	var moon = document.getElementById("moon");
+	moon.classList.toggle("gone");
+	var element = document.getElementById("toggle-btn");
+	element.classList.toggle("right");
+	var elem = document.getElementById("toggle");
+	elem.classList.toggle("right");
+}
